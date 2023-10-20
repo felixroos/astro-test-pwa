ba2ef1c1c1b347f5c1669a5b82afe37a4305e975
Felix Roos <flix91@gmail.com>
2023-10-20 23:44:30 +0200

    fix: base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,5 @@ export default defineConfig({
   build: {
     format: "directory",
   },
+  base: "astro-test-pwa",
 });
--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/astro-test-pwa/sw.js', { scope: '/astro-test-pwa/' })})}
